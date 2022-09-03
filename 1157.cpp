2732f5537153b5a3bf1b8d7703286bb1e72e4d4f
AnikHaque <ekramul15-811@diu.edu.bd>
2022-09-03 11:15:18 -0500

    deleted

--- 1157.cpp
+++ 1157.cpp
@@ -1,8 +1,4 @@
-// https://www.beecrowd.com.br/judge/en/problems/view/1157
-// Tanjila Shamima
-// Software Engineer at BJIT Group
-// uri online problem solved by Tanjila Shamima
-// personal website: https://tanjila-shamima.web.app/
+
 
 #include <iostream>
 #include <iomanip>
